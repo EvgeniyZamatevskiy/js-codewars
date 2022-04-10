cd6e97539b00e892c0b66b7a0a03a33c6e542aa8
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-04-10 15:53:34 +0300

    task 25

---
+++ tasks/25.js
@@ -0,0 +1,8 @@
+var isAnagram = function (test, original) {
+   test = test.toLowerCase().split('').sort().join('')
+   original = original.toLowerCase().split('').sort().join('')
+
+   return test === original
+}
+
+console.log(isAnagram('Twoo', 'WooT')) // true
