adf1df63dedabfa394a791f0cac64270325946db
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-04-10 12:19:19 +0300

    task 12

--- tasks/11.js
+++ tasks/11.js
@@ -3,10 +3,6 @@ let haystack_2 = ['283497238987234', 'a dog', 'a cat', 'some random junk', 'a pi
 let haystack_3 = [1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, 'needle', 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54];
 
 function findNeedle(haystack) {
-   for (let i = 0; i < haystack.length; i++) {
-      if (haystack[i] === 'needle') {
-         return `found the needle at position ${i}`
-      }
-   }
+   return `found the needle at position ${haystack.indexOf('needle')}`
 }
-console.log(findNeedle(haystack_3))
+console.log(findNeedle(haystack_3)) // found the needle at position 30
---
+++ tasks/12.js
@@ -0,0 +1,17 @@
+function sumArray(array) {
+   if (array === null) {
+      return 0
+   } else if (array.length < 2) {
+      return 0
+   } else {
+      array = array.sort((a, b) => {
+         return a - b
+      })
+      let total = 0
+      for (let i = 1; i < array.length - 1; i++) {
+         total = total + array[i]
+      }
+      return total
+   }
+}
+console.log(sumArray([1, 2, 3, 4]))
